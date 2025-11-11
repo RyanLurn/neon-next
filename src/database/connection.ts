d713aa51fb9a49aa5e90a1e3d99fe60051353172
Ryan Luong <lumintriet@gmail.com>
2025-11-11 18:47:56 +0700

    chore(db): register identity tables to db connection schema

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,9 +1,11 @@
 import { drizzle } from "drizzle-orm/neon-http";
 
 import { serverEnvironmentVariables } from "@/lib/env/server";
+import * as identityTables from "@/database/schema/identity";
 
 const database = drizzle({
   connection: serverEnvironmentVariables.NEON_POOLED_CONNECTION_STRING,
+  schema: { ...identityTables },
 });
 
 export { database };

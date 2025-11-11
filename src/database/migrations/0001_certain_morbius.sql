4761e0410f648b2664c182dae658f4caf1e0e4b1
Ryan Luong <lumintriet@gmail.com>
2025-11-11 19:22:25 +0700

    chore(db): run migration to create indexes

---
+++ src/database/migrations/0001_certain_morbius.sql
@@ -0,0 +1,3 @@
+CREATE INDEX "accounts_user_id_idx" ON "accounts" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "sessions_user_id_idx" ON "sessions" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "verifications_identifier_idx" ON "verifications" USING btree ("identifier");

545a3241b8f7b95574ce89a2a3ad1e1167978469
Ryan Luong <lumintriet@gmail.com>
2025-11-11 10:36:17 +0700

    fix(style): disable perfectionist for schema objects

--- src/database/schema/identity.ts
+++ src/database/schema/identity.ts
@@ -1,13 +1,14 @@
+/* eslint-disable perfectionist/sort-objects */
 import { boolean, pgTable, text } from "drizzle-orm/pg-core";
 
 import { primaryIdentifier } from "@/database/helpers/primary-identifier";
 import { timestamps } from "@/database/helpers/timestamps";
 
 const userTable = pgTable("users", {
-  emailVerified: boolean("email_verified").default(false).notNull(),
-  email: text("email").notNull().unique(),
-  name: text("name").notNull(),
   id: primaryIdentifier,
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  emailVerified: boolean("email_verified").default(false).notNull(),
   image: text("image"),
   ...timestamps,
 });

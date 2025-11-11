5b16e770fa93a15c8fd830174e2220ebc5a68221
Ryan Luong <lumintriet@gmail.com>
2025-11-11 19:18:41 +0700

    chore(db): add indexes to tables

--- src/database/helpers/foreign-keys/user-id.ts
+++ src/database/helpers/foreign-keys/user-id.ts
@@ -2,8 +2,8 @@ import { uuid } from "drizzle-orm/pg-core";
 
 import { userTable } from "@/database/schema/identity";
 
-const userIdForeignKey = uuid("user_id")
+const userId = uuid("user_id")
   .notNull()
   .references(() => userTable.id, { onDelete: "cascade" });
 
-export { userIdForeignKey };
+export { userId };
--- src/database/schema/identity.ts
+++ src/database/schema/identity.ts
@@ -1,8 +1,8 @@
 /* eslint-disable perfectionist/sort-objects */
-import { timestamp, boolean, pgTable, text } from "drizzle-orm/pg-core";
+import { timestamp, boolean, pgTable, index, text } from "drizzle-orm/pg-core";
 
-import { userIdForeignKey } from "@/database/helpers/foreign-keys/user-id";
 import { primaryIdentifier } from "@/database/helpers/primary-identifier";
+import { userId } from "@/database/helpers/foreign-keys/user-id";
 import { timestamps } from "@/database/helpers/timestamps";
 
 const userTable = pgTable("users", {
@@ -14,49 +14,61 @@ const userTable = pgTable("users", {
   ...timestamps,
 });
 
-const sessionTable = pgTable("sessions", {
-  id: primaryIdentifier,
-  userIdForeignKey,
-  token: text("token").notNull().unique(),
-  expiresAt: timestamp("expires_at", {
-    mode: "date",
-    withTimezone: true,
-  }).notNull(),
-  ipAddress: text("ip_address"),
-  userAgent: text("user_agent"),
-  ...timestamps,
-});
+const sessionTable = pgTable(
+  "sessions",
+  {
+    id: primaryIdentifier,
+    userId,
+    token: text("token").notNull().unique(),
+    expiresAt: timestamp("expires_at", {
+      mode: "date",
+      withTimezone: true,
+    }).notNull(),
+    ipAddress: text("ip_address"),
+    userAgent: text("user_agent"),
+    ...timestamps,
+  },
+  (table) => [index("user_id_idx").on(table.userId)]
+);
 
-const accountTable = pgTable("accounts", {
-  id: primaryIdentifier,
-  userIdForeignKey,
-  accountId: text("account_id").notNull(),
-  providerId: text("provider_id").notNull(),
-  accessToken: text("access_token"),
-  refreshToken: text("refresh_token"),
-  accessTokenExpiresAt: timestamp("access_token_expires_at", {
-    mode: "date",
-    withTimezone: true,
-  }),
-  refreshTokenExpiresAt: timestamp("refresh_token_expires_at", {
-    mode: "date",
-    withTimezone: true,
-  }),
-  scope: text("scope"),
-  idToken: text("id_token"),
-  password: text("password"),
-  ...timestamps,
-});
+const accountTable = pgTable(
+  "accounts",
+  {
+    id: primaryIdentifier,
+    userId,
+    accountId: text("account_id").notNull(),
+    providerId: text("provider_id").notNull(),
+    accessToken: text("access_token"),
+    refreshToken: text("refresh_token"),
+    accessTokenExpiresAt: timestamp("access_token_expires_at", {
+      mode: "date",
+      withTimezone: true,
+    }),
+    refreshTokenExpiresAt: timestamp("refresh_token_expires_at", {
+      mode: "date",
+      withTimezone: true,
+    }),
+    scope: text("scope"),
+    idToken: text("id_token"),
+    password: text("password"),
+    ...timestamps,
+  },
+  (table) => [index("user_id_idx").on(table.userId)]
+);
 
-const verificationTable = pgTable("verifications", {
-  id: primaryIdentifier,
-  identifier: text("identifier").notNull(),
-  value: text("value").notNull(),
-  expiresAt: timestamp("expires_at", {
-    mode: "date",
-    withTimezone: true,
-  }).notNull(),
-  ...timestamps,
-});
+const verificationTable = pgTable(
+  "verifications",
+  {
+    id: primaryIdentifier,
+    identifier: text("identifier").notNull(),
+    value: text("value").notNull(),
+    expiresAt: timestamp("expires_at", {
+      mode: "date",
+      withTimezone: true,
+    }).notNull(),
+    ...timestamps,
+  },
+  (table) => [index("identifier_idx").on(table.identifier)]
+);
 
 export { verificationTable, accountTable, sessionTable, userTable };

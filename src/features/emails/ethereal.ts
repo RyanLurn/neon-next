cead21a428904475f9d4869f7fd99702b75429c6
Ryan Luong <lumintriet@gmail.com>
2025-11-12 15:54:12 +0700

    feat(emails): configure Ethereal transport for development

---
+++ src/features/emails/ethereal.ts
@@ -0,0 +1,26 @@
+import "server-only";
+import { createTestAccount, createTransport } from "nodemailer";
+
+let cachedTransporter: Awaited<ReturnType<typeof createTransport>> | undefined;
+
+async function createEtherealTransport() {
+  if (cachedTransporter) {
+    return cachedTransporter;
+  }
+
+  const testAccount = await createTestAccount();
+  const transporter = createTransport({
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+    host: "smtp.ethereal.email",
+    secure: false,
+    port: 587,
+  });
+
+  cachedTransporter = transporter;
+  return cachedTransporter;
+}
+
+export { createEtherealTransport };

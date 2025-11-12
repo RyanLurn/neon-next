1d9e6c01cec20e9d24efa488e550e2e556bb60a4
Ryan Luong <lumintriet@gmail.com>
2025-11-12 10:11:29 +0700

    feat(auth): SignIn page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,55 @@
+"use client";
+
+import { useState } from "react";
+
+import {
+  CardDescription,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  Card,
+} from "@/components/ui/card";
+import { SignInForm } from "@/features/auth/components/forms/sign-in";
+import { ErrorAlert } from "@/components/utilities/error-alert";
+
 export default function SignInPage() {
-  return <div>Sign In</div>;
+  const [serverError, setServerError] = useState<undefined | string>();
+
+  function showServerError(errorMessage: string) {
+    setServerError(errorMessage);
+  }
+
+  function clearServerError() {
+    if (serverError) {
+      setServerError(undefined);
+    }
+  }
+
+  return (
+    <div className="flex size-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <Card>
+          <CardHeader>
+            <CardTitle>Sign In</CardTitle>
+            <CardDescription>
+              Enter your credentials below to sign in
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            {serverError && (
+              <ErrorAlert
+                errorTitle="Unable to sign in"
+                errorMessage={serverError}
+                className="mb-6"
+              />
+            )}
+            <SignInForm
+              showServerError={showServerError}
+              onFocus={clearServerError}
+            />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }

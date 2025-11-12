ab29dcd1f7ac6c23ca5bd9e915ef5398f4c1671d
Ryan Luong <lumintriet@gmail.com>
2025-11-12 16:32:02 +0700

    feat(auth): send verification email configuration

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -2,6 +2,8 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 import { betterAuth } from "better-auth";
 
+import type { EmailType } from "@/features/emails/types";
+
 import {
   verificationTable,
   sessionTable,
@@ -9,9 +11,26 @@ import {
   userTable,
 } from "@/database/schema/identity";
 import { serverEnvironmentVariables } from "@/lib/env/server";
+import { sendEmail } from "@/features/emails/send";
 import { database } from "@/database/connection";
 
 const auth = betterAuth({
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url }) => {
+      const verificationEmail: EmailType = {
+        html: `<p>Please click this link to verify your email: <a href="${url}" target="_blank">${url}</a></p>`,
+        text: `Please click this link to verify your email: ${url}`,
+        subject: "Verify your email address",
+        from: "support@neonrain.com",
+        to: user.email,
+      };
+      const sendEmailResult = await sendEmail(verificationEmail);
+
+      if (sendEmailResult.isErr()) {
+        throw new Error(sendEmailResult.error.message);
+      }
+    },
+  },
   database: drizzleAdapter(database, {
     schema: {
       verification: verificationTable,

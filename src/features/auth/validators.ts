0e97463775be2b2c7c85ae5e60c16657dacd5bb6
Ryan Luong <lumintriet@gmail.com>
2025-11-12 08:45:52 +0700

    feat(auth): email password validators

---
+++ src/features/auth/validators.ts
@@ -0,0 +1,13 @@
+import * as z from "zod";
+
+const nameValidator = z
+  .string()
+  .min(3, "Name must be at least 3 characters long");
+
+const emailValidator = z.email("Invalid email");
+
+const passwordValidator = z
+  .string()
+  .min(8, "Password must be at least 8 characters long");
+
+export { passwordValidator, emailValidator, nameValidator };

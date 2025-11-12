927e93b07c14aa2517c6c821f7cadb6ee709ea38
Ryan Luong <lumintriet@gmail.com>
2025-11-12 18:17:34 +0700

    feat(auth): resend verification link instructions

---
+++ src/features/auth/components/verification/resend-instructions.tsx
@@ -0,0 +1,22 @@
+import { ResendButton } from "@/features/auth/components/verification/resend-button";
+
+function ResendInstructions() {
+  return (
+    <div className="mt-4 flex flex-col gap-y-3">
+      <p className="text-lg font-semibold">
+        Cannot find the verification email we sent you?
+      </p>
+      <p className="leading-7">Please follow these steps:</p>
+      <ol className="mb-2 ml-6 list-disc">
+        <li>Check your spam/junk folder</li>
+        <li>
+          If you still cannot find it, click the button below. We'll send you a
+          different email with a new link.
+        </li>
+      </ol>
+      <ResendButton />
+    </div>
+  );
+}
+
+export { ResendInstructions };

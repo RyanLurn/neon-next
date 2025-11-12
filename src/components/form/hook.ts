b9082475591e9566eb4a4ac4d39fe849c84e370d
Ryan Luong <lumintriet@gmail.com>
2025-11-12 08:51:03 +0700

    feat(ui): add form submit button to app form hook

--- src/components/form/hook.ts
+++ src/components/form/hook.ts
@@ -2,10 +2,11 @@ import { createFormHook } from "@tanstack/react-form";
 
 import { FormTextInputField } from "@/components/form/fields/text-input";
 import { fieldContext, formContext } from "@/components/form/context";
+import { FormSubmitButton } from "@/components/form/submit-button";
 
 const { withFieldGroup, useAppForm, withForm } = createFormHook({
   fieldComponents: { FormTextInputField },
-  formComponents: {},
+  formComponents: { FormSubmitButton },
   fieldContext,
   formContext,
 });

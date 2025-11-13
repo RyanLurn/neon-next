e942bbded80c031d434792583eef531bb30c28ce
Ryan Luong <lumintriet@gmail.com>
2025-11-13 07:56:42 +0700

    feat(auth): add GitHub OAuth credentials

--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -3,9 +3,11 @@ import * as z from "zod";
 
 const serverEnvironmentVariables = createEnv({
   server: {
+    GITHUB_CLIENT_SECRET: z.string().min(1),
     NEON_POOLED_CONNECTION_STRING: z.url(),
     NEON_DIRECT_CONNECTION_STRING: z.url(),
     BETTER_AUTH_SECRET: z.string().min(1),
+    GITHUB_CLIENT_ID: z.string().min(1),
     RESEND_API_KEY: z.string().min(1),
     BETTER_AUTH_URL: z.url(),
   },

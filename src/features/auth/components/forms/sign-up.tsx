3c645f24fa5aa67e75c40f8b8e37885b69ded029
Ryan Luong <lumintriet@gmail.com>
2025-11-13 10:28:47 +0700

    feat(auth): implement 2 views for sign up form

--- src/features/auth/components/forms/sign-up.tsx
+++ src/features/auth/components/forms/sign-up.tsx
@@ -1,8 +1,9 @@
-import type { ComponentProps, FormEvent } from "react";
 import type { Route } from "next";
 
+import { type ComponentProps, type FormEvent, Activity, useState } from "react";
 import { useStore } from "@tanstack/react-form";
 import { useRouter } from "next/navigation";
+import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
 import {
@@ -13,10 +14,12 @@ import {
   Field,
 } from "@/components/ui/field";
 import { PasswordConfirmFieldGroup } from "@/features/auth/components/password-confirm-field-group";
+import { ContinueWithEmail } from "@/features/auth/components/continue-with-email";
 import { emailValidator, nameValidator } from "@/features/auth/validators";
 import { OAuthOptions } from "@/features/auth/components/oauth/options";
 import { useAppForm } from "@/components/form/hook";
 import { authClient } from "@/features/auth/client";
+import { Button } from "@/components/ui/button";
 
 interface SignUpFormProperties extends ComponentProps<"form"> {
   showServerError: (errorMessage: string) => void;
@@ -24,6 +27,9 @@ interface SignUpFormProperties extends ComponentProps<"form"> {
 
 function SignUpForm({ showServerError, ...properties }: SignUpFormProperties) {
   const router = useRouter();
+
+  const [view, setView] = useState<"social" | "email">("social");
+
   const signUpForm = useAppForm({
     onSubmit: async ({ value }) => {
       const { error } = await authClient.signUp.email({
@@ -46,10 +52,19 @@ function SignUpForm({ showServerError, ...properties }: SignUpFormProperties) {
       name: "",
     },
   });
+
   const isSubmitting = useStore(
     signUpForm.store,
     (state) => state.isSubmitting
   );
+
+  function showSocialButtons() {
+    setView("social");
+  }
+
+  function showEmailForm() {
+    setView("email");
+  }
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -59,69 +74,94 @@ function SignUpForm({ showServerError, ...properties }: SignUpFormProperties) {
   return (
     <form {...properties} onSubmit={(event) => void handleSubmit(event)}>
       <FieldSet>
-        <OAuthOptions
-          showServerError={showServerError}
-          isSubmitting={isSubmitting}
-        />
-        <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
-          Or continue with
-        </FieldSeparator>
-        <FieldGroup>
-          <signUpForm.AppField
-            children={(field) => (
-              <field.FormTextInputField
-                placeholder="Anders Hejlsberg"
-                disabled={isSubmitting}
-                label="Full Name"
-                type="text"
-                required
-              />
-            )}
-            validators={{
-              onChange: nameValidator,
-            }}
-            name="name"
+        <Activity mode={view === "social" ? "visible" : "hidden"}>
+          <OAuthOptions
+            showServerError={showServerError}
+            isSubmitting={isSubmitting}
           />
-          <signUpForm.AppField
-            children={(field) => (
-              <field.FormTextInputField
-                placeholder="example@gmail.com"
-                disabled={isSubmitting}
-                label="Email"
-                type="email"
-                required
-              />
-            )}
-            validators={{
-              onChange: emailValidator,
-            }}
-            name="email"
-          />
-          <PasswordConfirmFieldGroup
-            fields={{
-              confirmPassword: "confirmPassword",
-              password: "password",
-            }}
-            confirmPasswordLabel="Confirm Password"
-            passwordLabel="Password"
-            disabled={isSubmitting}
-            form={signUpForm}
-          />
-        </FieldGroup>
-        <FieldGroup>
-          <signUpForm.AppForm>
-            <signUpForm.FormSubmitButton
-              submittingText="Signing up..."
-              text="Create Account"
-            />
-          </signUpForm.AppForm>
+          <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+            Or continue with
+          </FieldSeparator>
           <Field>
-            <FieldDescription className="px-6 text-center">
-              <span>Already have an account?</span>{" "}
-              <Link href="/sign-in">Sign in</Link>
-            </FieldDescription>
+            <ContinueWithEmail showEmailForm={showEmailForm} />
           </Field>
-        </FieldGroup>
+        </Activity>
+        <Activity mode={view === "email" ? "visible" : "hidden"}>
+          <FieldGroup>
+            <signUpForm.AppField
+              children={(field) => (
+                <field.FormTextInputField
+                  placeholder="Anders Hejlsberg"
+                  disabled={isSubmitting}
+                  label="Full Name"
+                  type="text"
+                  required
+                />
+              )}
+              validators={{
+                onChange: nameValidator,
+              }}
+              name="name"
+            />
+            <signUpForm.AppField
+              children={(field) => (
+                <field.FormTextInputField
+                  placeholder="example@gmail.com"
+                  disabled={isSubmitting}
+                  label="Email"
+                  type="email"
+                  required
+                />
+              )}
+              validators={{
+                onChange: emailValidator,
+              }}
+              name="email"
+            />
+            <PasswordConfirmFieldGroup
+              fields={{
+                confirmPassword: "confirmPassword",
+                password: "password",
+              }}
+              confirmPasswordLabel="Confirm Password"
+              passwordLabel="Password"
+              disabled={isSubmitting}
+              form={signUpForm}
+            />
+          </FieldGroup>
+          <FieldGroup className="gap-y-3">
+            <signUpForm.AppForm>
+              <signUpForm.FormSubmitButton
+                submittingText="Signing up..."
+                text="Create Account"
+              />
+            </signUpForm.AppForm>
+            <signUpForm.Subscribe
+              selector={(state) => ({
+                isSubmitting: state.isSubmitting,
+              })}
+            >
+              {({ isSubmitting }) => (
+                <Button
+                  aria-disabled={isSubmitting}
+                  onClick={showSocialButtons}
+                  disabled={isSubmitting}
+                  variant="link"
+                  type="button"
+                >
+                  <ArrowLeft />
+                  <span>Back to other options</span>
+                </Button>
+              )}
+            </signUpForm.Subscribe>
+            <Field>
+              <FieldDescription className="px-6 text-center">
+                <span>Already have an account?</span>{" "}
+                <Link href="/sign-in">Sign in</Link>
+              </FieldDescription>
+            </Field>
+          </FieldGroup>
+        </Activity>
       </FieldSet>
     </form>
   );
